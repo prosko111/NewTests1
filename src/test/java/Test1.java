35b0631c9b1744a80fb3a2b844e6c79e91f7dfa8
prosko111 <prosko111@yandex.ru>
2021-11-30 11:50:49 +0300

    Base work

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -18,7 +19,21 @@ public class Test1 {
         driver.findElement(By.id("login-username")).sendKeys("po4ta@nepravv.ru");
         driver.findElement(By.id("login-password")).sendKeys("igogogo43");
 
-        driver.findElement(By.xpath("//input[@type=\"checkbox\"]")).click();
+        driver.findElement(By.xpath("//*[@class=\"checkbox\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();  // Нажатие кнопки Лог Ин, локатор Xpath (по тексту)
+
+        WebElement errorMessage = driver.findElement(By.xpath("//span[@class=\"ng-binding ng-scope\"]"));
+        String strText = errorMessage.getText();
+        String strExpected1 = "Your request failed. Please try again.";
+        String strExpected2 = "Неправильное имя пользователя или пароль.";
+        String strActual = strText;
+        System.out.println(strActual);
+        if (strExpected1.equals(strActual) || strExpected2.equals(strActual)) {
+            System.out.println("Строки равны");
+        }
+        else {
+            System.out.println("Строки не равны");
+        }
+        driver.close();
     }
 }
